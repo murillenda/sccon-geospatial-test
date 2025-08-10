6604cf3aadbb7162ba8652dd1a1e8c4e49a00ed0
Murillo Marques <giikthelol@gmail.com>
2025-08-10 16:43:23 -0300

    Initial config

---
+++ src/main/java/com/github/murillenda/sccon/geospatial/ScconGeospatialTestApplication.java
@@ -0,0 +1,13 @@
+package com.github.murillenda.sccon.geospatial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScconGeospatialTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScconGeospatialTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/murillenda/sccon/geospatial/ScconGeospatialTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.murillenda.sccon.geospatial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScconGeospatialTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
